569ac531081041a5cf61b018cf67a58ba857e955
Ian Shepard <ianshepard99@gmail.com>
2019-03-11 16:08:12 -0400

    add javaFX display of parsed weather data

--- src/main/java/edu/bsu/cs/Main.java
+++ src/main/java/edu/bsu/cs/Main.java
@@ -1,12 +1,17 @@
 package edu.bsu.cs;
 
+import com.google.gson.Gson;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main extends Application {
 
@@ -14,16 +19,23 @@ public class Main extends Application {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private WeatherPoints getSampleWeatherPointsAsJson() {
+        InputStream sampleInputStream =
+                getClass().getClassLoader().getResourceAsStream("PointsJsonExample");
+        BufferedReader sampleIn = new BufferedReader(new InputStreamReader(sampleInputStream));
+        return new Gson().fromJson(sampleIn, WeatherPoints.class);
+    }
 
-        }
-        Scene scene = new Scene(root);
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        WeatherPoints weatherPoints = getSampleWeatherPointsAsJson();
+        Parser parser = new Parser();
+        ArrayList<Period> exampleForecast = parser.getCurrentForecast(weatherPoints);
+        HashMap<String, String> forecastToday = exampleForecast.get(0).getForecast();
+
+        String displayString = forecastToday.get("name") + "\nTemperature: " + forecastToday.get("temperature") + "°" + forecastToday.get("temperatureUnit") + "\nWind Speed is " + forecastToday.get("windSpeed") + " going " + forecastToday.get("windDirection") + "\nCloud cover is " + forecastToday.get("shortForecast");
+        Label test = new Label(displayString);
+        Scene scene = new Scene(test);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
---
+++ src/test/java/edu/bsu/cs/testImage.java
@@ -0,0 +1,34 @@
+package edu.bsu.cs;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class testImage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException {
+        FileInputStream input = new FileInputStream("C:\\Users\\Ian\\Pictures\\Wiki page data src.png");
+        Image image = new Image(input);
+        ImageView viewer = new ImageView();
+        viewer.setImage(image);
+
+        HBox hbox = new HBox(viewer);
+        Scene scene = new Scene(hbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
